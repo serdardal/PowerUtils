5227bea49770c756c68347503eceb23e514e4569
Serdar Dal <serdardal9807@gmail.com>
2023-02-12 22:25:19 +0300

    ps utils initial: Get-KeyValues function

---
+++ PowerUtils.psm1
@@ -0,0 +1,29 @@
+﻿function Get-KeyValues {
+	param(
+		[Parameter(Mandatory=$true)]
+		[string]$Path
+	)
+	
+	$pairs = @()
+	
+	$content = @(Get-Content -Path $Path -Encoding 'UTF8')
+	
+	for($i = 0; $i -lt $content.Count; $i++){
+		$line = $content[$i]
+		$seperatorIndex = $line.IndexOf('=');
+		
+		if($seperatorIndex -eq -1){
+			$errLine = $i + 1
+			Throw "Key-Value seperator (=) not found! Line: $errLine"
+		}
+		
+		$pairs += [PSCustomObject]@{
+			Key = $line.SubString(0, $seperatorIndex)
+			Value = $line.SubString($seperatorIndex + 1, $line.Length - $seperatorIndex - 1)
+		}
+	}
+	
+	return ,$pairs
+}
+
+Export-ModuleMember -Function Get-KeyValues
