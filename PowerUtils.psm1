0b3293661901eb603b99d4185f6411166cf161f8
Serdar Dal <serdardal9807@gmail.com>
2023-02-14 19:52:21 +0300

    set-keyvalue function added

--- PowerUtils.psm1
+++ PowerUtils.psm1
@@ -10,6 +10,12 @@
 	
 	for($i = 0; $i -lt $content.Count; $i++){
 		$line = $content[$i]
+		$line = $line.Trim()
+		
+		if($line -eq ''){
+			continue
+		}
+		
 		$seperatorIndex = $line.IndexOf('=');
 		
 		if($seperatorIndex -eq -1){
@@ -24,4 +30,41 @@
 	}
 	
 	return ,$pairs
+}
+
+function Set-KeyValue {
+	param(
+		[Parameter(Mandatory=$true)]
+		[string]$Path,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Key,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Value
+	)
+	
+	if(-Not (Test-Path -Path $Path)){
+		Set-Content -Path $Path -Value $null -Encoding 'UTF8'
+	}
+	
+	$lines = @(Get-Content -Path $Path -Encoding 'UTF8')
+	
+	$settingLineIdx = $null
+	for($i = 0; $i -lt $lines.Count; $i++){
+		$line = $lines[$i]
+		if($line.StartsWith("$($Key)=")){
+			$settingLineIdx = $i
+			break
+		}
+	}
+	
+	if($settingLineIdx -eq $null){
+		$lines += "$($Key)=$($Value)"
+	}
+	else{
+		$lines[$settingLineIdx] = "$($Key)=$($Value)"
+	}
+	
+	Set-Content -Path $Path -Value $lines -Encoding 'UTF8'
 }
